bede9a11411a6dd148712f7ce64cbe64f6d5469d
DESKTOP-QPSS3P8\Nathan Squire <nathansquire67@gmail.com>
2016-09-09 08:04:13 +1000

    First actual implementation of the site

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+function onLoad(){
+	resize();
+	reWrite('initial');
+}
 function resize(){
 	var width = window.innerWidth;
 	var body = document.getElementsByTagName('body')[0];
 
-	if(width < 1000){
+	if(width < 1100){
 		body.classList.add('smaller');
+		console.log("smaller");
 	}else{
 		body.classList.remove('smaller');
+		console.log("larger");
+	}
+}
+
+function reWrite(option){
+	var sampleText = document.getElementsByClassName('home')[0].getElementsByTagName('p')[0];
+	switch (option) {
+		case 'about':
+			sampleText.innerHTML = "Dusty Vinyl is a small graphics and design studio specialising in creating characteristic album art, booklets, CD cases, and websites for the music industry.";
+			break;
+		case 'contact':
+			sampleText.innerHTML = "We'd love to hear from you. Email us at dustyvinyl@gmail.com";
+			break;
+		default:
+			sampleText.innerHTML = " ";
 	}
 }
