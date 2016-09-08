09a8bbfaf6a217b1b7e1274288b7b3070b8a6e45
DESKTOP-QPSS3P8\Nathan Squire <nathansquire67@gmail.com>
2016-09-09 09:01:52 +1000

    Meta tags, fancy javascript

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 function onLoad(){
 	resize();
-	reWrite('initial');
+	reWrite('about');
 }
+
 function resize(){
 	var width = window.innerWidth;
 	var body = document.getElementsByTagName('body')[0];
 
 	if(width < 1100){
 		body.classList.add('smaller');
-		console.log("smaller");
 	}else{
 		body.classList.remove('smaller');
-		console.log("larger");
 	}
 }
 
 function reWrite(option){
-	var sampleText = document.getElementsByClassName('home')[0].getElementsByTagName('p')[0];
+	var sampleText = document.getElementById('sampleText');
+	var listItems = document.getElementsByClassName('home')[0].getElementsByTagName('a');
+	for(i=0; i< listItems.length; i++){
+		listItems[i].classList.remove('highlighted');
+	}
 	switch (option) {
 		case 'about':
+			var title = document.getElementById('about');
+			title.classList.add('highlighted');
 			sampleText.innerHTML = "Dusty Vinyl is a small graphics and design studio specialising in creating characteristic album art, booklets, CD cases, and websites for the music industry.";
 			break;
 		case 'contact':
+			var title = document.getElementById('contact');
+			title.classList.add('highlighted');
 			sampleText.innerHTML = "We'd love to hear from you. Email us at dustyvinyl@gmail.com";
 			break;
 		default:
